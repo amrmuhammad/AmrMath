339e8539c6fef89ae929571c59ea6df0c05c192e
Amr Muhammad <amrmuhammad@amrmuhammad.com>
2025-09-21 19:39:58 +0300

    Added exponents10

--- godot/amrmath/math_content.gd
+++ godot/amrmath/math_content.gd
@@ -29,6 +29,7 @@ var textures = [
 	preload("res://math_content_textures/exponents7.png"),
 	preload("res://math_content_textures/exponents8.png"),
 	preload("res://math_content_textures/exponents9.png"),
+	preload("res://math_content_textures/exponents10.png")
 ]
 
 func _ready():
